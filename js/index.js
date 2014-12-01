4362f8d3949c99f288df334d221af9a8e3fec7c5
ojack <ojackson@gmail.com>
2014-12-01 14:15:36 -0500

    added favicon, minor edits

--- js/index.js
+++ js/index.js
@@ -5,13 +5,18 @@ $(function() {
 	$(".filter").click(function(){
 		//$(".tt-grid li").css({"display": "none"});
 		$(".tt-grid li").addClass("hide");
+		$(".filter").removeClass("highlight");
 		//$("."+$(this).text()).css({"display": "inline-block"});
 		$("."+$(this).text()).removeClass("hide");
+		$(this).addClass("highlight");
+		$("#show-all").removeClass("highlight");
 		//alert($(this).text());
 	});
 
 	$("#show-all").click(function(){
 		//$(".tt-grid li").css({"display": "inline-block"});
+		$(".filter").removeClass("highlight");
+		$(this).addClass("highlight");
 		$(".tt-grid li").removeClass("hide");
 	});
 
